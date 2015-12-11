6f7acbfc3989dfc0b9f0b224a385fb78bba87b6a
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2015-12-11 16:26:27 +0900

    add reverse playback

--- lib/renchin/cli.rb
+++ lib/renchin/cli.rb
@@ -23,7 +23,7 @@ module Renchin
       # Split a movie to png images.
       system("ffmpeg -i #{movie_file} -f image2 #{image_directory_path}/%7d.#{ext}")
       # Generate timelapse movie from frame images.
-      system("ffmpeg -f image2 -r #{playback_time(ext)} -i #{image_directory_path}/%7d.#{ext} -r #{output_fps} -an -vcodec libx264 -pix_fmt yuv420p #{result_file}")
+      system("ffmpeg -f image2 -r #{frame_per_second(ext)} -i #{image_directory_path}/%7d.#{ext} -r #{output_fps} -an -vcodec libx264 -pix_fmt yuv420p #{result_file}")
 
       delete_directory(image_directory_path, "\.#{ext}")
       say("Renchin generated timelapse! ~> #{result_file}", :green)
@@ -46,12 +46,30 @@ module Renchin
       say("Renchin generated sprite file! ~> #{result_file}", :green)
     end
 
+    desc "reverse -i MOVIE_FILE -o MOVIE_FILE -start START_TIME -end END_TIME", "Generate a reverse playback movie"
+    method_option :input, aliases: "-i", desc: "Input movie file path"
+    method_option :output, aliases: "-o", desc: "Output result file"
+    method_option :start, aliases: "-s", desc: "Movie start time"
+    method_option :end, aliases: "-e", desc: "Movie end time"
+    def reverse
+      movie_file = options[:input]
+      result_file = options[:output]
+      start_sec = options[:start] || 0
+      end_sec = options[:end] || 10
+
+      image_directory_path = image_directory(__method__)
+
+      system("ffmpeg -i #{movie_file} -vf trim=#{start_sec}:#{end_sec},reverse,setpts=PTS-STARTPTS  -an #{result_file}")
+
+      say("Renchin generated reverse movie! ~> #{result_file}", :green)
+    end
+
     private
     def frame_count(expr)
       Dir.glob(expr).count
     end
 
-    def playback_time(ext)
+    def frame_per_second(ext)
       frame_count("*\.#{ext}") > 10 ? frame_count("*\.#{ext}") / 10 : 1
     end
   end
